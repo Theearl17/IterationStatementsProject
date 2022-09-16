0e978651f5e7c707f25741099bf3b3fa94a49f0f
earl starks <earlstarks@rocketmail.com>
2022-09-16 11:39:16 -0500

    IterationStatements

---
+++ Desktop/repos/IterationStatements/IterationStatements/IterationStatements/Program.cs
@@ -0,0 +1,43 @@
+﻿//Exercise 1 : For 
+
+for (int i = 0; i < 1; i++)
+{
+    Console.WriteLine($"{1}");
+    Console.WriteLine($"{2}");
+    Console.WriteLine($"{3}");
+    Console.WriteLine($"{4}");
+    Console.WriteLine($"{5}");
+    Console.WriteLine($"{6}");
+    Console.WriteLine($"{7}");
+    Console.WriteLine($"{8}");
+    Console.WriteLine($"{9}");
+    Console.WriteLine($"{10}");
+    Console.WriteLine($"{11}");
+    Console.WriteLine($"{12}");
+    Console.WriteLine($"{13}");
+    Console.WriteLine($"{14}");
+    Console.WriteLine($"{15}");
+}
+
+// Exercise 2 : Do-While Loop
+
+do
+{
+
+} while (true);
+
+
+// Exercise 3 : While Loop
+
+while (true)
+{
+
+}
+
+// Exercise 4 : Foreach Loop
+
+foreach (var item in collection)
+
+{
+
+}
---
+++ Desktop/repos/MethodExercise/MethodExercise/MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("What is your first name?");// input 
+var userName = Console.ReadLine();
+
+Console.WriteLine($"Hi,{userName}. What is your favorite color?"); // input
+var color = Console.ReadLine();
+
+Console.WriteLine($"{color} is sketch. What is your favorite animal?");// input
+var animal = Console.ReadLine();
+
+Console.WriteLine($"{animal} is the best! What is your favorite band?");// input
+var band = Console.ReadLine();
+
+Console.WriteLine($"{band} is sketch as well?"); //input
+
+
+
+
+
